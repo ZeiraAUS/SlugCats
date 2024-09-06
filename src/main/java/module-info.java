17e9a52d679dd21c74b3f85662463cfa669127ed
Jack Yeomans <jjyeom@hotmail.com>
2024-09-06 14:42:39 +1000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.addressbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.SlugCats to javafx.fxml;
+    exports com.SlugCats;
+    exports com.SlugCats.controller;
+    opens com.SlugCats.controller to javafx.fxml;
+    exports com.SlugCats.model;
+    opens com.SlugCats.model to javafx.fxml;
+}
