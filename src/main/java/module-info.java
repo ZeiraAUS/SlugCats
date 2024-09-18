1254bfd38482e40c878c0874d45c44a139b3e700
JC123321 <chengjiayiwa14@gmail.com>
2024-09-18 15:16:29 +1000

    reverts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,14 @@ module com.example.addressbook {
     requires javafx.fxml;
     requires java.sql;
     requires org.xerial.sqlitejdbc;
+    exports auth;
+    exports NewAuth;
 
 
     opens com.SlugCats to javafx.fxml;
     exports com.SlugCats;
     exports com.SlugCats.controller;
     opens com.SlugCats.controller to javafx.fxml;
-    exports com.SlugCats.model;
+    exports com.SlugCats.Models;
     opens com.SlugCats.model to javafx.fxml;
 }
