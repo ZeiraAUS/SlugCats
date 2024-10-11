08cec453c690595888169dbaa93896ff3634f5e8
JC123321 <chengjiayiwa14@gmail.com>
2024-10-11 14:54:03 +1000

    Reset password working

--- src/main/java/com/SlugCats/NewAuth/Change.java
+++ src/main/java/com/SlugCats/NewAuth/Change.java
@@ -4,7 +4,7 @@ import com.SlugCats.DAOs.*;
 import com.SlugCats.Models.User;
 
 public class Change {
-    static boolean resetpassword(String username,String password,String ConfirmPassword){// not complete
+    public static boolean resetpassword(String username,String password,String ConfirmPassword){// not complete
         if (!password.equals(ConfirmPassword)) {
             System.out.println("Password and confirm password do not match.");
             return false;
@@ -16,11 +16,12 @@ public class Change {
             System.out.println("User not found.");
             return false;
         }
+        System.out.println("User found.");
         String salt = Hash_salt.getSalt();
         String hashedPassword=Hash_salt.generateHash(password, salt);
 
-        user.changePassword(password);
-
+        user.changePassword(hashedPassword);
+        userDAO.UpdateUser(user);
 
         return true;
     }
