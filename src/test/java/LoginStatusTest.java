d174111c4915b4a2bb2867c47e242b36d87e8c4f
JC123321 <chengjiayiwa14@gmail.com>
2024-10-19 01:11:45 +1000

    fix

--- src/test/java/LoginStatusTest.java
+++ src/test/java/LoginStatusTest.java
@@ -1,23 +1,39 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.SlugCats.NewAuth.login_status;
+import com.SlugCats.NewAuth.register;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.SlugCats.Models.User;
 
-public class LoginStatusTest {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class LoginStatusTest {
+    private com.SlugCats.NewAuth.register register;
     private login_status loginStatus;
     private User testUser;
 
     @BeforeEach
     public void setUp() {
         loginStatus = new login_status();
+        this.register= new register();
         // Assuming User class has a constructor that takes username and password
 
     }
+    @Test
+    public void testRegisterUserExists() throws SQLException {
+        // two password not equal
+        assertFalse(register.register_a_user("testUser",  "password123", "password13","a","a","asa"));
+    }
 
+    @Test
+
+    public void testRegisterSuccess() throws SQLException {
+        assertTrue(register.register_a_user("NewUser",  "password123", "password123","a","asa","asa"));
+
+    }
 
     @Test
     public void testLoginSuccess() {
@@ -40,3 +56,5 @@ public class LoginStatusTest {
     }
 
 }
+
+
