48effa55dac4e702fce131d267c9cc04cde1d6c5
Sketchist <amy_chieng@hotmail.com>
2024-10-25 14:02:54 +1000

    fixed mackenzie dumb dumb statistics view error

--- src/main/java/com/SlugCats/controller/StatisticsController.java
+++ src/main/java/com/SlugCats/controller/StatisticsController.java
@@ -1,7 +1,6 @@
 package com.SlugCats.controller;
 
 import com.SlugCats.Models.GameTime;
-import com.SlugCats.gamestracking.GameDetector;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
@@ -10,10 +9,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import com.SlugCats.DAOs.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -27,13 +24,11 @@ public class StatisticsController {
     @FXML
     private ImageView logoImage;
     @FXML
-    private Button backButton, selectButton;
+    private Button backButton;
     @FXML
-    private Label statisticsLabel, dailyStatLabel, monthlyStatLabel, yearlyStatLabel, gameLabel, gameStatLabel;
+    private Label statisticsLabel, dailyStatLabel, monthlyStatLabel, yearlyStatLabel;
     @FXML
     private Tab dailyTab, monthlyTab, yearlyTab;
-
-    private String displayName;
 
     // Class for configuring controller components.
     private Components components = new Components();
@@ -60,23 +55,10 @@ public class StatisticsController {
         dailyStatLabel.setPadding(tabPadding);
         monthlyStatLabel.setPadding(tabPadding);
         yearlyStatLabel.setPadding(tabPadding);
-        components.setButtonImage(selectButton,"/images/folder.PNG",25);
-        HBox selectGameBox = new HBox();
-        selectGameBox.getChildren().addAll(
-                selectButton,
-                gameLabel
-        );
-        VBox gameTabBox = new VBox();
-        gameStatLabel.setPadding(new Insets(15.0,0.0,0.0,0.0));
-        gameTabBox.getChildren().addAll(
-                selectGameBox,
-                gameStatLabel
-        );
-        gameTabBox.setPadding(tabPadding);
         TabPane statisticsPane = new TabPane();
         statisticsPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         statisticsPane.setTabMinHeight(200.0);
-        statisticsPane.setTabMinWidth(215);
+        statisticsPane.setTabMinWidth(290);
         statisticsPane.getTabs().addAll(
                 dailyTab,
                 monthlyTab,
@@ -93,39 +75,6 @@ public class StatisticsController {
     protected void onBackButtonClick() throws IOException {
         components.changeView(backButton,"home-view.fxml");
     }
-
-    /**
-     * Allows user to select a specific game to view the statistics of.
-     * @throws IOException
-     */
-    @FXML
-    protected void onSelectButtonClick() throws IOException {
-        GameDetector gameDetector = new GameDetector();
-        File selectedFile = gameDetector.choosefile();
-
-        String selectedGameTitle = "No Game Detected";
-        if (selectedFile != null && selectedFile.exists()) {
-            String gameName = selectedFile.getName();
-
-            int lastDotIndex = gameName.lastIndexOf('.');
-            if (lastDotIndex != -1) {
-                displayName = gameName.substring(0, lastDotIndex);
-            } else {
-                displayName = gameName;
-            }
-
-            // Spaces for visual clarity.
-            selectedGameTitle = "   Selected Game: " + displayName;
-        } else {
-            selectedGameTitle = "   Selected Game: No Game Detected";
-        }
-
-        // Update the label to show just the game name
-        gameLabel.setText(selectedGameTitle);
-    }
-
-    // NOTE TO TEAM: So these methods detect when a tab has been selected.
-    // Put logic here to load in the statistics for each tab. You can use \n for the labels for next line as well.
 
     /**
      * When user selects the daily tab, display statistics for that day.
@@ -196,6 +145,10 @@ public class StatisticsController {
         yearlyStatLabel.setText(yearlyStats.toString());
     }
 
+    /**
+     * Get statistics for day tab.
+     * @return
+     */
     private List<GameTime> GetGameTimesThisDay()
     {
         List<GameTime> sortedGameTimeList = new ArrayList<>();
@@ -213,6 +166,10 @@ public class StatisticsController {
         return sortedGameTimeList;
     }
 
+    /**
+     * Get statistics for month tab.
+     * @return
+     */
     private List<GameTime> GetGameTimesThisMonth()
     {
         List<GameTime> sortedGameTimeList = new ArrayList<>();
@@ -230,6 +187,10 @@ public class StatisticsController {
         return sortedGameTimeList;
     }
 
+    /**
+     * Get statistics for year tab.
+     * @return
+     */
     private List<GameTime> GetGameTimesThisYear()
     {
         List<GameTime> sortedGameTimeList = new ArrayList<>();
